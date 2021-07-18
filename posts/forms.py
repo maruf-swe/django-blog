524196aef1e870a09190c8879559f938a4bf9bc2
Maruf <marufur1847@gmail.com>
2021-07-18 23:17:48 +0600

    add requirments

--- posts/forms.py
+++ posts/forms.py
@@ -18,16 +18,17 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'overview', 'content', 'thumbnail',
-                  'categories','featured', 'previous_post', 'next_post')
+                  'categories', 'featured', 'previous_post', 'next_post')
 
 
 class CommentForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={
         'class': 'form-control',
         'placeholder': 'Type your comment here',
-        'id':'usercomment',
+        'id': 'usercomment',
         'rows': 4
     }))
+
     class Meta:
         model = Comment
         fields = ('content',)
