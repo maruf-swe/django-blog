9ea4aeb822e82b27d25ea07e1c7c6658672cf969
Maruf <marufur1847@gmail.com>
2021-07-18 15:17:50 +0600

    add register,login,logout urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -6,9 +7,13 @@ from django.urls import path, include
 from posts.views import (index, blog, post_detail,
                          search, post_create,
                          post_update, post_delete)
+from user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('', index, name='home'),
     path('blog/', blog, name='post-list'),
     path('search/', search, name='search'),
