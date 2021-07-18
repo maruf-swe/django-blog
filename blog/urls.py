3d3ee04a4b7fdc1ebc9b2e025db49894744f4998
Maruf <marufur1847@gmail.com>
2021-07-18 17:11:31 +0600

    index page edit

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,7 @@ from user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
+    #path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('', index, name='home'),
